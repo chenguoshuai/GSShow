965ce5eaee962f0c8f82e0a89c8afa33523232e6
GSChan <ChanGuoShuai@ChenGuoShuai.local>
2017-01-06 15:41:59 +0800

    初始化项目

---
+++ GSShow/GSShow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GSShow
+//
+//  Created by GSChan on 2017/1/6.
+//  Copyright © 2017年 GS Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GSShow/GSShow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GSShow
+//
+//  Created by GSChan on 2017/1/6.
+//  Copyright © 2017年 GS Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
